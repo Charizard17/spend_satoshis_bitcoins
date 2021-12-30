c2ecba32e38e03a51e9231298575a3b133ed7492
Esad Dursun <3723esad@gmail.com>
2021-12-30 14:30:19 +0100

    dummy product images changed

--- lib/widgets/product_list.dart
+++ lib/widgets/product_list.dart
@@ -9,37 +9,37 @@ class ProductList extends StatelessWidget {
       id: 'p1',
       title: 'Hamburger',
       price: 5.00,
-      imageUrl: 'https://im5.ezgif.com/tmp/ezgif-5-abb4360219.jpg',
+      imageUrl: 'https://media.istockphoto.com/photos/simple-meat-burger-isolated-on-white-picture-id157696235',
     ),
     Product(
       id: 'p2',
       title: 'Camera',
       price: 850.00,
-      imageUrl: 'https://im5.ezgif.com/tmp/ezgif-5-9b1a5049cd.jpg',
+      imageUrl: 'https://images.unsplash.com/photo-1502920917128-1aa500764cbd?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=700&q=80',
     ),
     Product(
       id: 'p3',
       title: 'Car',
       price: 30000.00,
-      imageUrl: 'https://im5.ezgif.com/tmp/ezgif-5-1cc7d5befe.jpg',
+      imageUrl: 'https://images.unsplash.com/photo-1511919884226-fd3cad34687c?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=700&q=80',
     ),
     Product(
       id: 'p4',
       title: 'House',
       price: 150000.00,
-      imageUrl: 'https://im5.ezgif.com/tmp/ezgif-5-2a13fe148e.jpg',
+      imageUrl: 'https://images.unsplash.com/photo-1600596542815-ffad4c1539a9?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=700&q=80',
     ),
     Product(
       id: 'p5',
       title: 'Yacht',
       price: 1100000.00,
-      imageUrl: 'https://im5.ezgif.com/tmp/ezgif-5-89bb5fcaf7.jpg',
+      imageUrl: 'https://media.istockphoto.com/photos/motorboat-picture-id157693170',
     ),
     Product(
       id: 'p6',
       title: 'Plane',
       price: 15000000.00,
-      imageUrl: 'https://im5.ezgif.com/tmp/ezgif-5-e101c74a62.jpg',
+      imageUrl: 'https://media.istockphoto.com/photos/airbus-a320-aeroplane-picture-id171264813',
     ),
   ];
 
