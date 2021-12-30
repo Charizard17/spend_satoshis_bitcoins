f960673ceadb270cb0f6baaef9e99ccdc4c44bd7
Esad Dursun <3723esad@gmail.com>
2021-12-30 12:08:38 +0100

    product model created

---
+++ lib/models/product.dart
@@ -0,0 +1,13 @@
+class Product {
+  final String id;
+  final String title;
+  final double price;
+  final String imageUrl;
+
+  Product(
+    this.id,
+    this.title,
+    this.price,
+    this.imageUrl,
+  );
+}
