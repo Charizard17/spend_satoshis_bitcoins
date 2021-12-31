0ae8ffe70ffcc431b2e11d984afb3f813667ad15
Esad Dursun <3723esad@gmail.com>
2021-12-31 18:34:37 +0100

    crypto icons package installed. btc icon added to menu drawer

--- lib/widgets/menu_drawer.dart
+++ lib/widgets/menu_drawer.dart
@@ -1,5 +1,6 @@
 import 'package:flutter/material.dart';
 import 'package:provider/provider.dart';
+import 'package:crypto_font_icons/crypto_font_icons.dart';
 
 import '../provider/currencies.dart';
 
@@ -7,6 +8,7 @@ class MenuDrawer extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     final currency = Provider.of<Currencies>(context, listen: true);
+    bool _isDollar = currency.isDollar;
 
     return Drawer(
       child: Column(
@@ -22,8 +24,12 @@ class MenuDrawer extends StatelessWidget {
             onTap: () {},
           ),
           ListTile(
-            leading: Icon(Icons.attach_money),
-            title: Text('Change currency ₿ <=> \$'),
+            leading: _isDollar == true
+                ? Icon(Icons.attach_money)
+                : Icon(CryptoFontIcons.BTC),
+            title: _isDollar == true
+                ? Text('Change currency \$ ⇆ ₿')
+                : Text('Change currency ₿ ⇆ \$'),
             onTap: currency.changeCurrency,
           ),
         ],
