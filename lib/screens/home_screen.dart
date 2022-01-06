6ad7777ba2335e96179b2f6ca49ee198c899b845
Esad Dursun <3723esad@gmail.com>
2022-01-06 19:04:01 +0100

    url_launcher package installed. home_screen basically finished. get latest currency price function implemented

--- lib/screens/home_screen.dart
+++ lib/screens/home_screen.dart
@@ -1,22 +1,89 @@
+import 'package:flutter/gestures.dart';
 import 'package:flutter/material.dart';
+import 'package:url_launcher/url_launcher.dart';
+import 'package:provider/provider.dart';
 
 import './spend_screen.dart';
+import '../provider/currencies.dart';
 
 class HomeScreen extends StatelessWidget {
   static const routeName = '/home-screen';
 
   @override
   Widget build(BuildContext context) {
+    final currency = Provider.of<Currencies>(context, listen: true);
+
     return Scaffold(
-      appBar: AppBar(
-        title: Text('Home'),
-      ),
-      body: Center(
-        child: TextButton(
-          child: Text('Go to spend screen..'),
-          onPressed: () {
-            Navigator.of(context).pushNamed(SpendScreen.routeName);
-          },
+      body: SafeArea(
+        child: Container(
+          child: Column(
+            children: [
+              Image.asset(
+                'assets/satoshi-nakamoto.png',
+              ),
+              Padding(
+                padding:
+                    const EdgeInsets.symmetric(horizontal: 20, vertical: 30),
+                child: RichText(
+                  text: TextSpan(
+                    style: TextStyle(color: Colors.black, fontSize: 16),
+                    children: [
+                      TextSpan(
+                        text: 'According to a report by Whale Alert, ',
+                      ),
+                      TextSpan(
+                        text: 'Satoshi Nakamoto',
+                        style: TextStyle(
+                          fontWeight: FontWeight.bold,
+                        ),
+                      ),
+                      TextSpan(
+                        text: ' had mined ',
+                      ),
+                      TextSpan(
+                        text: '1,125,150',
+                        style: TextStyle(
+                          fontWeight: FontWeight.bold,
+                        ),
+                      ),
+                      TextSpan(
+                        text:
+                            ' tokens up to block 54,316 in the chain, making the person/persons the most productive miner in the world. ',
+                      ),
+                      TextSpan(
+                        text: 'Source',
+                        style: TextStyle(
+                          color: Colors.blue,
+                          decoration: TextDecoration.underline,
+                        ),
+                        recognizer: TapGestureRecognizer()
+                          ..onTap = () async {
+                            await launch(
+                                'https://www.cnbctv18.com/cryptocurrency/satoshi-nakamotos-bitcoin-holding-heres-how-much-it-is-worth-now-11608832.htm');
+                          },
+                      ),
+                    ],
+                  ),
+                ),
+              ),
+              ElevatedButton(
+                child: Text(
+                  'Start spending',
+                  style: TextStyle(
+                    color: Colors.black,
+                    fontSize: 17,
+                  ),
+                ),
+                style: ElevatedButton.styleFrom(
+                  primary: Theme.of(context).primaryColor,
+                ),
+                onPressed: () {
+                  currency.getLatestPrice();
+                  Navigator.of(context).pushNamed(SpendScreen.routeName);
+                },
+              ),
+            ],
+          ),
         ),
       ),
     );
