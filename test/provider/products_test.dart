c9981ab2ee495a7b6c7689a8fb835960a08eb9a6
Esad Dursun <3723esad@gmail.com>
2022-01-11 17:19:56 +0100

    products test file edited

--- test/provider/products_test.dart
+++ test/provider/products_test.dart
@@ -2,11 +2,9 @@ import 'package:flutter_test/flutter_test.dart';
 import 'package:spend_satoshis_bitcoins/provider/products.dart';
 
 void main() {
-  test('Initialize product list', () {
+  test('Products added manually. If a new product is added, the number (32) needs to be increased', () {
     final _products = Products();
 
-    // products added manually
-    // if a new product is added, this number (32) needs to be increased
     expect(_products.products.length, 32);
   });
 }
