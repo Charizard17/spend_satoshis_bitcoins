b145a363bc729d9955274df58194f27a6f736d8f
Esad Dursun <3723esad@gmail.com>
2022-01-11 17:13:47 +0100

    unit testing for products provider

---
+++ test/provider/products_test.dart
@@ -0,0 +1,11 @@
+import 'package:flutter_test/flutter_test.dart';
+import 'package:spend_satoshis_bitcoins/provider/products.dart';
+
+void main() {
+  test('Initialize product list', () {
+    final _products = Products();
+
+    // If a new product is added, this number (32) needs to be increased
+    expect(_products.products.length, 32);
+  });
+}
